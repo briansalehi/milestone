8a74a9ea598cda953e797b86c623308f66a88031
Brian Salehi <salehibrian@gmail.com>
2021-02-25 14:22:50 +0330

    darkmode toggling button created

--- share/js/darkmode.js
+++ share/js/darkmode.js
@@ -1,11 +1,28 @@
-function darkmode(manual) {
-	const hours = new Date().getHours();
-	const isNight = !(hours > 6 && hours < 20);
+function darkmode() {
 	var bodystyle = document.body;
-	if (isNight || manual) {
+	const dark = sessionStorage.getItem('darkmode');
+	console.log(dark);
+
+	if (dark === 'true' || dark == 'undefined') {
 		bodystyle.classList.add("dark-mode");
+		sessionStorage.setItem('darkmode', true);
 	} else {
+		sessionStorage.setItem('darkmode', false);
 		bodystyle.classList.remove("dark-mode");
 	}
 }
 darkmode();
+
+function toggle_darkmode() {
+	var bodystyle = document.body;
+	const dark = sessionStorage.getItem('darkmode');
+	console.log(dark);
+
+	if (dark === 'true' || dark == 'undefined') {
+		bodystyle.classList.remove("dark-mode");
+		sessionStorage.setItem('darkmode', false);
+	} else {
+		sessionStorage.setItem('darkmode', true);
+		bodystyle.classList.add("dark-mode");
+	}
+}
