cbd2d5acc96346bfd0c9d3332ff2ae3af28a921b
Brian Salehi <salehibrian@gmail.com>
2021-03-14 15:24:49 +0330

    smooth scrolling behavior implemented

--- share/js/scroll-listener.js
+++ share/js/scroll-listener.js
@@ -1,10 +1,13 @@
+var current_position = 0;
+
 function scrollDownListener() {
   window.addEventListener(
     "keydown",
     function(event) {
       if (event.key == "j") {
         var screen_height = window.screen.height / 4;
-        window.scrollTo({top: screen_height, behavior: 'smooth'})
+		current_position += screen_height;
+        window.scrollTo({top: current_position, behavior: 'smooth'})
       }
     }
   )
@@ -16,7 +19,11 @@ function scrollUpListener() {
     function(event) {
       if (event.key == "k") {
         var screen_height = window.screen.height / 4;
-        window.scrollTo({top: -screen_height, behavior: 'smooth'})
+		current_position -= screen_height;
+		if (current_position < 0) {
+			current_position = 0;
+		}
+        window.scrollTo({top: current_position, behavior: 'smooth'})
       }
     }
   )
