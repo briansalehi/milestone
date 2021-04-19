0d1afa0cc599178922edbb9c9250550265e6eaf0
Brian Salehi <salehibrian@gmail.com>
2021-04-19 14:25:11 +0430

    add dynamic font size for all elements

--- share/js/fontsize.js
+++ share/js/fontsize.js
@@ -1,5 +1,5 @@
 var minimum_fontlevel = 1;
-var maximum_fontlevel = 10;
+var maximum_fontlevel = 12;
 var current_fontlevel = localStorage.getItem('fontlevel');
 if (current_fontlevel == null) {
 	current_fontlevel = 3;
@@ -23,16 +23,16 @@ function zoominLetter() {
 			localStorage.setItem('fontsize', s);
 		}
 
-		var code = document.getElementsByTagName('code');
-		for(i=0;i<code.length;i++) {
-			if(code[i].style.fontSize) {
-				var s = parseInt(code[i].style.fontSize.replace("px",""));
+		var pre = document.getElementsByTagName('pre');
+		for(i=0;i<pre.length;i++) {
+			if(pre[i].style.fontSize) {
+				var s = parseInt(pre[i].style.fontSize.replace("px",""));
 			} else {
 				var s = 14;
 			}
 			s += 2;
 
-			code[i].style.fontSize = s+"px"
+			pre[i].style.fontSize = s+"px"
 		}
 
 		var li = document.getElementsByTagName('li');
@@ -57,6 +57,57 @@ function zoominLetter() {
 			s += 2;
 
 			table[i].style.fontSize = s+"px"
+		}
+
+		var cite = document.getElementsByTagName('cite');
+		for(i=0;i<cite.length;i++) {
+			if(cite[i].style.fontSize) {
+				var s = parseInt(cite[i].style.fontSize.replace("px",""));
+			} else {
+				var s = 14;
+			}
+			s += 2;
+
+			cite[i].style.fontSize = s+"px"
+		}
+
+		var h2 = document.getElementsByTagName('h2');
+		for(i=0;i<h2.length;i++) {
+			if(h2[i].style.fontSize) {
+				var s = parseInt(h2[i].style.fontSize.replace("px",""));
+			} else {
+				var s = 30;
+			}
+			s += 2;
+
+			h2[i].style.fontSize = s+"px"
+			localStorage.setItem('h2-fontsize', s);
+		}
+
+		var h3 = document.getElementsByTagName('h3');
+		for(i=0;i<h3.length;i++) {
+			if(h3[i].style.fontSize) {
+				var s = parseInt(h3[i].style.fontSize.replace("px",""));
+			} else {
+				var s = 24;
+			}
+			s += 2;
+
+			h3[i].style.fontSize = s+"px"
+			localStorage.setItem('h3-fontsize', s);
+		}
+
+		var h4 = document.getElementsByTagName('h4');
+		for(i=0;i<h4.length;i++) {
+			if(h4[i].style.fontSize) {
+				var s = parseInt(h4[i].style.fontSize.replace("px",""));
+			} else {
+				var s = 20;
+			}
+			s += 2;
+
+			h4[i].style.fontSize = s+"px"
+			localStorage.setItem('h4-fontsize', s);
 		}
 	}
 }
@@ -79,17 +130,16 @@ function zoomoutLetter() {
 			localStorage.setItem('fontsize', s);
 		}
 
-		var code = document.getElementsByTagName('code');
-		for(i=0;i<code.length;i++) {
-			if(code[i].style.fontSize) {
-				var s = parseInt(code[i].style.fontSize.replace("px",""));
+		var pre = document.getElementsByTagName('pre');
+		for(i=0;i<pre.length;i++) {
+			if(pre[i].style.fontSize) {
+				var s = parseInt(pre[i].style.fontSize.replace("px",""));
 			} else {
 				var s = 14;
 			}
 			s -= 2;
 
-			code[i].style.fontSize = s+"px"
-			localStorage.setItem('fontsize', s);
+			pre[i].style.fontSize = s+"px"
 		}
 
 		var li = document.getElementsByTagName('li');
@@ -102,7 +152,6 @@ function zoomoutLetter() {
 			s -= 2;
 
 			li[i].style.fontSize = s+"px"
-			localStorage.setItem('fontsize', s);
 		}
 
 		var table = document.getElementsByTagName('table');
@@ -115,29 +164,102 @@ function zoomoutLetter() {
 			s -= 2;
 
 			table[i].style.fontSize = s+"px"
-			localStorage.setItem('fontsize', s);
+		}
+
+		var cite = document.getElementsByTagName('cite');
+		for(i=0;i<cite.length;i++) {
+			if(cite[i].style.fontSize) {
+				var s = parseInt(cite[i].style.fontSize.replace("px",""));
+			} else {
+				var s = 14;
+			}
+			s -= 2;
+
+			cite[i].style.fontSize = s+"px"
+		}
+
+		var h2 = document.getElementsByTagName('h2');
+		for(i=0;i<h2.length;i++) {
+			if(h2[i].style.fontSize) {
+				var s = parseInt(h2[i].style.fontSize.replace("px",""));
+			} else {
+				var s = 30;
+			}
+			s -= 2;
+
+			h2[i].style.fontSize = s+"px"
+			localStorage.setItem('h2-fontsize', s);
+		}
+
+		var h3 = document.getElementsByTagName('h3');
+		for(i=0;i<h3.length;i++) {
+			if(h3[i].style.fontSize) {
+				var s = parseInt(h3[i].style.fontSize.replace("px",""));
+			} else {
+				var s = 24;
+			}
+			s -= 2;
+
+			h3[i].style.fontSize = s+"px"
+			localStorage.setItem('h3-fontsize', s);
+		}
+
+		var h4 = document.getElementsByTagName('h4');
+		for(i=0;i<h4.length;i++) {
+			if(h4[i].style.fontSize) {
+				var s = parseInt(h4[i].style.fontSize.replace("px",""));
+			} else {
+				var s = 24;
+			}
+			s -= 2;
+
+			h4[i].style.fontSize = s+"px"
+			localStorage.setItem('h4-fontsize', s);
 		}
 	}
 }
 
 function restoreFontSize() {
 	var fontsize = localStorage.getItem('fontsize');
+	if (fontsize == null)
+		return;
+
+	var h2fontsize = localStorage.getItem('h2-fontsize');
+	var h3fontsize = localStorage.getItem('h3-fontsize');
+	var h4fontsize = localStorage.getItem('h4-fontsize');
+
 	var p = document.getElementsByTagName('p');
-	var code = document.getElementsByTagName('code');
+	var pre = document.getElementsByTagName('pre');
 	var li = document.getElementsByTagName('li');
 	var table= document.getElementsByTagName('table');
+	var cite= document.getElementsByTagName('cite');
+	var h2= document.getElementsByTagName('h2');
+	var h3= document.getElementsByTagName('h3');
+	var h4= document.getElementsByTagName('h4');
 
 	for(i=0;i<p.length;i++) {
 		p[i].style.fontSize = fontsize+"px"
 	}
-	for(i=0;i<code.length;i++) {
-		code[i].style.fontSize = fontsize+"px"
+	for(i=0;i<pre.length;i++) {
+		pre[i].style.fontSize = fontsize+"px"
 	}
 	for(i=0;i<li.length;i++) {
 		li[i].style.fontSize = fontsize+"px"
 	}
 	for(i=0;i<table.length;i++) {
 		table[i].style.fontSize = fontsize+"px"
+	}
+	for(i=0;i<cite.length;i++) {
+		cite[i].style.fontSize = fontsize+"px"
+	}
+	for(i=0;i<h2.length;i++) {
+		h2[i].style.fontSize = h2fontsize+"px"
+	}
+	for(i=0;i<h3.length;i++) {
+		h3[i].style.fontSize = h3fontsize+"px"
+	}
+	for(i=0;i<h4.length;i++) {
+		h4[i].style.fontSize = h4fontsize+"px"
 	}
 }
 restoreFontSize();
