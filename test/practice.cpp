09a8d2e9610d3e3bc45d8b411b802105c9ed1527
Brian Salehi <salehibrian@gmail.com>
2022-12-23 22:10:45 +0330

    add first tests

---
+++ test/practice.cpp
@@ -0,0 +1,27 @@
+#include <gtest/gtest.h>
+#include <practice.hpp>
+
+TEST(PracticeTest, ConstructionTests)
+{
+    flashback::practice default_constructed;
+    ASSERT_EQ(default_constructed.question().empty(), true);
+}
+
+TEST(PracticeTest, CopyConstructionTest)
+{
+    flashback::practice default_constructed;
+    flashback::practice copy_constructed(default_constructed);
+    ASSERT_EQ(copy_constructed.question().empty(), true);
+}
+
+TEST(PracticeTest, MoveConstructionTest)
+{
+    flashback::practice move_constructed(flashback::practice{});
+    ASSERT_EQ(move_constructed.question().empty(), true);
+}
+
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
