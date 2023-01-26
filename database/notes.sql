46628d3ebc574ac6f11c43e8f1a58f53ebba1d79
Brian Salehi <salehibrian@gmail.com>
2023-01-26 16:50:38 +0330

    add extra columns to notes and resources table

--- database/notes.sql
+++ database/notes.sql
@@ -6,6 +6,7 @@ create table if not exists notes (
     position varchar(20),
     collected bool not null default false,
     collectable bool not null default true,
+    reference integer references reference_list(id),
     resource integer references resources(id),
     primary key (id)
 );
--- database/resources.sql
+++ database/resources.sql
@@ -4,6 +4,7 @@ create table if not exists resources (
     id integer generated always as identity,
     name varchar(15) not null,
     description varchar(120),
+    purchase_link varchar(2000),
     primary key (id)
 );
 
