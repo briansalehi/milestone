29961438179297076090dc50b4cb8e7bbc41ccdf
Brian Salehi <salehibrian@gmail.com>
2023-01-26 19:21:42 +0330

    remove incorrect relation of references to notes

--- database/notes.sql
+++ database/notes.sql
@@ -6,7 +6,6 @@ create table if not exists notes (
     position varchar(20),
     collected bool not null default false,
     collectable bool not null default true,
-    reference integer references reference_list(id),
     resource integer references resources(id),
     primary key (id)
 );
