90379399eea7253ec065e2c473c5356c882a38c2
Brian Salehi <salehibrian@gmail.com>
2023-01-27 12:21:50 +0330

    add description to importer

--- database/notes.sql
+++ database/notes.sql
@@ -3,6 +3,7 @@ begin work;
 create table if not exists notes (
     id integer generated always as identity,
     title varchar(10000) not null,
+    description text,
     position varchar(20),
     collected bool not null default false,
     collectable bool not null default true,
