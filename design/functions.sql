c2ba6f40d883e4fc7689711523021444b8198820
Brian Salehi <briansalehi@proton.me>
2024-07-25 23:28:29 +0200

    postgres: add create_resource function

--- design/functions.sql
+++ design/functions.sql
@@ -1,9 +1,13 @@
+drop procedure if exists create_note;
+drop procedure if exists create_resource;
+
+-- pre: there should be a temp_note_blocks temporary table
+-- post: temp_note_blocks temporary table will be cleared
 create or replace procedure create_note(
     resource_name varchar(1000),
     section_name varchar(100),
     heading varchar(400)
 )
-language plpgsql
 as $$
 declare resource_index integer;
 declare section_index integer;
@@ -15,9 +19,22 @@ begin
     select s.id into section_index from flashback.sections s where s.headline = section_name and s.resource_id = resource_index;
     insert into flashback.notes (resource_id, heading) values (resource_index, heading) returning id into note_index;
     insert into flashback.note_resources (note_id, section_id) values (note_index, section_index);
-    raise notice 'New note % in section % from resource % created', note_index, section_index, resource_index;
     insert into flashback.note_blocks (note_id, content, type, language) select note_index, t_content, t_type, t_language from temp_note_blocks;
     delete from temp_note_blocks;
-end;
-$$;
+end; $$ language plpgsql;
+
+-- pre: there should be a temp_sections temporary table
+-- post: temp_sections temporary table will be cleared
+create or replace procedure create_resource(
+    name_string varchar(2000),
+    type_string flashback.resource_type,
+    resource_reference varchar(2000) default null
+)
+as $$
+declare resource_index integer;
+begin
+    insert into flashback.resources (name, reference, type) values (name_string, resource_reference, type_string) returning id into resource_index;
+    insert into flashback.sections (resource_id, headline, reference) select resource_index, headline_string, reference_string from temp_sections;
+    delete from temp_sections;
+end; $$ language plpgsql;
 
--- design/modify.sql
+++ design/modify.sql
@@ -1,1 +1,2 @@
 alter table if exists flashback.resource_sections rename to sections;
+alter table if exists flashback.sections add column reference varchar(2000) default null;
