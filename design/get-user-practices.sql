93e3c7b4a38a0cf00f6d7558d0ca28b011f7cc3d
Brian Salehi <briansalehi@proton.me>
2024-07-25 06:51:19 +0200

    postgres: make functions for user studies and practices

--- design/get-user-practices.sql
+++ design/get-user-practices.sql
@@ -1,8 +1,56 @@
-select s.name as subject, t.name as topic, pr.heading as practice, string_agg(pb.content, E'\n\n') as content
-from flashback.practices pr
-join flashback.topics t on pr.topic_id = t.id
-join flashback.subjects s on s.id = t.subject_id
-left join flashback.practice_blocks pb on pb.practice_id = pr.id
-left join flashback.progress p on p.topic_id = t.id and p.user_id = 1
-group by s.id, s.name, t.id, t.name, pr.id, pr.heading, pr.id, p.updated
-order by p.updated desc nulls first, s.id, t.id, pr.id;
+-- subjects which user should practice
+create or replace function get_user_subjects(user_index integer)
+returns table (id integer, name varchar(50), topics bigint, updated timestamp)
+as $$
+begin
+    return query
+    select s.id, s.name, count(t.*) as topics, p.updated
+    from flashback.subjects s
+    join flashback.topics t on t.subject_id = s.id
+    left join flashback.progress p on p.topic_id = t.id and p.user_id = user_index
+    group by s.id, s.name, p.updated
+    order by p.updated desc nulls first, count(t.*) desc, s.id;
+end; $$ language plpgsql;
+
+-- topics of the subject chosen by user
+create or replace function get_user_topics(user_index integer, subject_index integer)
+returns table (subject_id integer, topic_id integer, name varchar(100), practices bigint, updated timestamp)
+as $$
+begin
+    return query
+    select s.id as subject_id, t.id as topic_id, t.name, count(pr.*) as practices, p.updated
+    from flashback.subjects s
+    join flashback.topics t on t.subject_id = s.id
+    join flashback.practices pr on pr.topic_id = t.id
+    left join flashback.progress p on p.topic_id = t.id and p.user_id = user_index
+    where s.id = subject_index
+    group by s.id, t.id, t.name, p.updated
+    order by p.updated desc nulls first, count(pr.*) desc, s.id, t.id;
+end; $$ language plpgsql;
+
+-- practices of the topic chosen by user
+create or replace function get_user_practices(user_index integer, topic_index integer)
+returns table (id integer, heading varchar(400), updated timestamp)
+as $$
+begin
+    return query
+    select pr.id, pr.heading, p.updated
+    from flashback.practices pr
+    left join flashback.progress p on p.topic_id = pr.topic_id and p.user_id = user_index
+    where pr.topic_id = topic_index
+    order by p.updated desc nulls first, pr.id;
+end; $$ language plpgsql;
+
+-- blocks of practices under user selected topic
+create or replace function get_user_practice_blocks(user_index integer, topic_index integer)
+returns table (practice_id integer, block_id integer, heading varchar(400), block text, type flashback.block_type, language varchar(10), updated timestamp)
+as $$
+begin
+    return query
+    select pr.id as practice_id, b.id as block_id, pr.heading, b.content, b.type, b.language, p.updated
+    from flashback.practices pr
+    join flashback.practice_blocks b on b.practice_id = pr.id
+    left join flashback.progress p on p.topic_id = pr.topic_id and p.user_id = user_index
+    where pr.topic_id = topic_index
+    order by p.updated desc nulls first, pr.id, b.id;
+end; $$ language plpgsql;
--- design/get-user-studies.sql
+++ design/get-user-studies.sql
@@ -1,21 +1,62 @@
-select r.id as resource_id, r.name as resource, count(sc.id) as incomplete_sections, max(st.updated) as last_update
-from flashback.resources r
-join flashback.sections sc on sc.resource_id = r.id and sc.state = 'writing'
-left join flashback.studies st on st.section_id = sc.id and st.user_id = 1
-group by r.id, r.name
-order by max(st.section_id) desc nulls first, r.id;
+drop function get_user_studying_resources;
+drop function get_user_studying_sections;
+drop function get_user_studying_notes;
+drop function get_user_studying_note_blocks;
 
-select sc.id, sc.headline, count(n.id) as notes, st.updated
-from flashback.sections sc
-join flashback.notes n on n.resource_id = sc.resource_id
-left join flashback.studies st on st.section_id = sc.id and st.user_id = 1
-where sc.resource_id = 15 and sc.state = 'writing'
-group by sc.id, sc.headline, st.updated
-order by st.updated desc nulls first, sc.id;
+-- resources that user is studying
+create or replace function get_user_studying_resources(user_index integer)
+returns table (resource_id integer, resource varchar(1000), incomplete_sections bigint, updated timestamp)
+as $$
+begin
+    return query
+    select r.id as resource_id, r.name as resource, count(sc.id) as incomplete_sections, max(st.updated)
+    from flashback.resources r
+    join flashback.sections sc on sc.resource_id = r.id and sc.state = 'writing'
+    left join flashback.studies st on st.section_id = sc.id and st.user_id = user_index
+    group by r.id, r.name
+    order by max(st.section_id) desc nulls first, r.id;
+end; $$ language plpgsql;
 
-select n.id, n.heading, n.state, n.updated, n.creation
-from flashback.sections sc
-join flashback.notes n on n.resource_id = sc.resource_id
-where sc.id = 4 and sc.state = 'writing'
-order by sc.id;
+-- writing resource sections which user is writing notes for
+create or replace function get_user_studying_sections(user_index integer, resource_index integer)
+returns table (id integer, headline varchar(100), notes bigint, updated timestamp)
+as $$
+begin
+    return query
+    select sc.id, sc.headline, count(n.id) as notes, st.updated
+    from flashback.sections sc
+    join flashback.notes n on n.resource_id = sc.resource_id
+    left join flashback.studies st on st.section_id = sc.id and st.user_id = user_index
+    where sc.resource_id = resource_index and sc.state = 'writing'
+    group by sc.id, sc.headline, st.updated
+    order by st.updated desc nulls first, sc.id;
+end; $$ language plpgsql;
 
+-- heading only notes written by user under a writing section
+create or replace function get_user_studying_notes(user_index integer, section_index integer)
+returns table (id integer, heading varchar(400), state flashback.state, updated timestamp, creation timestamp)
+as $$
+begin
+    return query
+    select n.id, n.heading, n.state, n.updated, n.creation
+    from flashback.sections sc
+    join flashback.note_resources ns on ns.section_id = sc.id
+    join flashback.notes n on n.id = ns.note_id
+    where sc.id = section_index and sc.state = 'writing'
+    order by sc.id;
+end; $$ language plpgsql;
+
+-- The full body of notes by which user is writing under a section
+create or replace function get_user_studying_note_blocks(user_index integer, section_index integer)
+returns table (note_id integer, block_id integer, content text, type flashback.block_type, language varchar(10), updated timestamp)
+as $$
+begin
+    return query
+    select n.id as note_id, nb.id as block_id, nb.content, nb.type, nb.language, nb.updated
+    from flashback.sections sc
+    join flashback.note_resources ns on ns.section_id = sc.id
+    join flashback.notes n on n.id = ns.note_id
+    join flashback.note_blocks nb on nb.note_id = n.id
+    where sc.id = section_index and sc.state = 'writing'
+    order by sc.id;
+end; $$ language plpgsql;
